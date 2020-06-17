f7312452d4ddcf64972ed4178b002edaad4aa781
leonidastheodoridis <ltheodor@hotmail.de>
2020-06-17 11:01:51 +0200

    94. Create the Project

---
+++ src/test/java/com/bharath/springjms/SpringjmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bharath.springjms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringjmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
